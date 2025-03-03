f51cfc93d4617a234a2b3846238d016d21210c3b
GS <gsaget@et.esiea.fr>
2025-03-04 00:05:33 +0100

    ajout fonctio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,10 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from "@/components/ThemeProvider";
 import Navbar from "@/components/Navbar";
-import { Sidebar } from "lucide-react";
-//import Sidebar from "@/components/Sidebar";
-//import { Toaster } from "react-hot-toast";
+import Sidebar from "@/components/Sidebar";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -43,17 +42,18 @@ export default function RootLayout({
               <Navbar />
 
               <main className="py-8">
-                {}
+                {/* container to center the content */}
                 <div className="max-w-7xl mx-auto px-4">
                   <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
                     <div className="hidden lg:block lg:col-span-3">
-                    <Sidebar/>
+                      <Sidebar />
                     </div>
                     <div className="lg:col-span-9">{children}</div>
                   </div>
                 </div>
               </main>
             </div>
+            <Toaster />
           </ThemeProvider>
         </body>
       </html>
